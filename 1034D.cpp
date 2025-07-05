28fa7f5318ec2b9e442f97608973249e36324406
PriyanshuNamadutt <namaduttpriyanshu@gmail.com>
2025-07-05 21:20:48 +0530

    Create 1034D.cpp

---
+++ 1034D.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int t; cin >> t;
+    while ( t-- ) {
+        int n, k;
+        cin >> n >> k;
+        string s; cin >> s;
+        int cnt = 0;
+        for ( int i = 0; i < n; i++ ) {
+            if ( s[i] == '1' ) cnt++;
+        }
+
+        cout << ( (cnt <= k) || ( n < 2*k )  ? "Alice" : "Bob" ) << endl;
+    }
+    return 0;
+}
